c9c168d725ba8ee087e2f391efbcdb9239c833c8
irving <zhouyongtao@outlook.com>
2014-12-15 10:00:11 +0800

    update AsyncLoggingFilter

--- webapi-learning/Filters/AsyncLoggingFilter.cs
+++ webapi-learning/Filters/AsyncLoggingFilter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
@@ -12,7 +13,37 @@ namespace webapi_learning.Filters
 {
     public class AsyncLoggingFilter : ActionFilterAttribute
     {
-
+        /*
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var actionDescriptor = filterContext.ActionDescriptor;
+            string controllerName = actionDescriptor.ControllerDescriptor.ControllerName;
+            string actionName = actionDescriptor.ActionName;
+            string userName = filterContext.HttpContext.User.Identity.Name.ToString();
+            DateTime timeStamp = filterContext.HttpContext.Timestamp;
+            string routeId = string.Empty;
+            if (filterContext.RouteData.Values["id"] != null)
+            {
+                routeId = filterContext.RouteData.Values["id"].ToString();
+            }
+            var message = new StringBuilder();
+            message.Append("UserName=");
+            message.Append(userName + "|");
+            message.Append("Controller=");
+            message.Append(controllerName + "|");
+            message.Append("Action=");
+            message.Append(actionName + "|");
+            message.Append("TimeStamp=");
+            message.Append(timeStamp.ToString() + "|");
+            if (!string.IsNullOrEmpty(routeId))
+            {
+                message.Append("RouteId=");
+                message.Append(routeId);
+            }
+            //logger.Log(message.ToString());
+            base.OnActionExecuted(filterContext);
+        } 
+        */
 
         /*
        	This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, 
--- webapi-learning/Models/Book.cs
+++ webapi-learning/Models/Book.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +9,8 @@ namespace webapi_learning.Models
 {
     public class Book
     {
+        [Display(Name="是否启用")]
+        [DefaultValue(true),Description("默认属性true")]
+        public bool Enable { get; set; }
     }
 }
